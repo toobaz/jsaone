f19349b4bb51983b1e9cb5fbc4b8f415ed39c491
Pietro Battiston <me@pietrobattiston.it>
2014-05-12 19:53:39 +0200

    long_description, description and classifiers

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from distutils.core import setup
 from Cython.Build import cythonize
 
+
+with open('README') as file:
+    long_description = file.read()
+
 setup(
   name = 'jsaone',
   version='0.1',
@@ -8,11 +12,12 @@ setup(
   author='Pietro Battiston',
   author_email='me@pietrobattiston.it',
   url='http://pietrobattiston.it/jsaone',
+  description='Incremental JSON parser',
+  long_description=long_description,
   ext_modules = cythonize("jsaone_cyt.pyx"),
   py_modules=['jsaone', 'jsaone_py'],
   classifiers=[
 	'Development Status :: 4 - Beta',
-	'Environment :: X11 Applications :: GTK',
 	'Intended Audience :: Developers',
 	'License :: OSI Approved :: GNU General Public License (GPL)',
 	'Programming Language :: Python',
