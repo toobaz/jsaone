cede34de485b504efadc9c29d4debcb70ef982f5
Pietro Battiston <me@pietrobattiston.it>
2016-11-06 19:04:49 +0100

    Cope with 3-fields version string

--- release_info.py
+++ release_info.py
@@ -4,3 +4,4 @@ app_name = 'jsaone'
 basefile = 'jsaone.py'
 files_to_substitute = ['jsaone.py']
 version_re = re.compile("__version__ *= *'([0-9]*\.[0-9]*(\.[0-9]*)?)'")
+version_re_setup = re.compile("version='([0-9]*\.[0-9]*(\.[0-9]*)?)',")
